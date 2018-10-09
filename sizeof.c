272d2b13525068784ee61b39495a19cf4f30a84b
Antonio De Santis <40795392+dravolo@users.noreply.github.com>
2018-10-09 15:27:21 +0200

    Update sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 10;
+    unsigned y = 150U;
+    long long int z = 150000LL;
+    _Bool b = 1;
+    char c = 'A';
+    double d = 10.50;
+    
+    printf("%d %d\n", x, sizeof(x));
+    printf("%d %d\n", y, sizeof(y));
+    printf("%d %d\n", z, sizeof(z));
+    printf("%d %d\n", b, sizeof(b));
+    printf("%c %d\n", c, sizeof(c));
+    printf("%f %d\n", d, sizeof(d));
+    return 0;
+}
